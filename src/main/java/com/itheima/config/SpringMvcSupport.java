ffdc0bab945648b73288af088c5026fdb196afbc
permanentct <3307617969@qq.com>
2024-07-13 17:13:22 +0800

    ssm操作项目

---
+++ src/main/java/com/itheima/config/SpringMvcSupport.java
@@ -0,0 +1,21 @@
+package com.itheima.config;
+
+import com.itheima.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class SpringMvcSupport extends WebMvcConfigurationSupport {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/students");
+    }
+}
